54f5ff9c79115ee4be439c4697f1198e463258ca
SRIKRISHNAN R <srikrishnan.raj@gmail.com>
2025-03-09 19:22:01 +0530

    index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter as Router } from 'react-router-dom'; // ✅ Import HashRouter
 import App from './App';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>  {/* ✅ Wrap App inside Router */}
+      <App />
+    </Router>
   </React.StrictMode>
 );
 
